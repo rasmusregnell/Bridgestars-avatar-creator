c9b9215ae01aaafa9d7d3603b031cc55c38e5ed6
rasmus regnell <rasmuserik2009@gmail.com>
2023-05-10 11:12:37 +0200

    created new class

--- Bridgestars/Util/AvatarUtil/Avatar.cs
+++ Bridgestars/Util/AvatarUtil/Avatar.cs
@@ -1,6 +1,15 @@
+using Bridgestars.Util.FeatureTypes;
+
+
+//using Test.AvatarGenerator.DesignParameters;
+
 namespace Bridgestars.Util.AvatarUtil;
 
 public class Avatar
 {
+    private Hair hairtype;
+    
+    //En metod som ger en alla typer, hair, clothesColor etc
+    
     
 }
